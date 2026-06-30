7329a636f1c69de12e02e5e4cf8cbae578f109cd
Nawaz Haider <nawazhaider60@gmail.com>
2026-07-01 00:47:59 +0600

    Code Formatted

--- src/architecture.py
+++ src/architecture.py
@@ -6,8 +6,6 @@ import torch.nn as nn
 from mamba_ssm import Mamba
 import torch.nn.functional as F
 from typing import List
-
-# Updated import for the new directory structure
 from config import ModelConfig
 
 
